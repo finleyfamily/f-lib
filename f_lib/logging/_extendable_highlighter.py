b939bcd36780da11f6549c174fff08c31ad2edc7
Kyle Finley <kyle@finley.sh>
2024-04-11 12:06:43 -0400

    add `aws.cloudformation` logging highlights (#24)

--- f_lib/logging/_extendable_highlighter.py
+++ f_lib/logging/_extendable_highlighter.py
@@ -40,6 +40,21 @@ class ExtendableHighlighter(Highlighter):
                 r"(?P<region>(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-\d)",
             ),
         ),
+        HighlightTypedDict(
+            base_style="aws.cloudformation.",
+            highlights=(
+                r"?P<capabilities>(CAPABILITY_IAM|CAPABILITY_NAMED_IAM|CAPABILITY_AUTO_EXPAND)",
+                r"?P<status>(CREATE_COMPLETE|CREATE_FAILED|CREATE_IN_PROGRESS|DELETE_COMPLETE|"
+                "DELETE_FAILED|DELETE_IN_PROGRESS|IMPORT_COMPLETE|IMPORT_IN_PROGRESS|"
+                "IMPORT_ROLLBACK_COMPLETE|IMPORT_ROLLBACK_FAILED|IMPORT_ROLLBACK_IN_PROGRESS|"
+                "REVIEW_IN_PROGRESS|ROLLBACK_COMPLETE|ROLLBACK_FAILED|ROLLBACK_IN_PROGRESS|"
+                "UPDATE_COMPLETE|UPDATE_COMPLETE_CLEANUP_IN_PROGRESS|UPDATE_FAILED|"
+                "UPDATE_IN_PROGRESS|UPDATE_ROLLBACK_COMPLETE|"
+                "UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS|UPDATE_ROLLBACK_FAILED|"
+                "UPDATE_ROLLBACK_IN_PROGRESS)",
+                r"?P<detailed_status>(CONFIGURATION_COMPLETE|VALIDATION_FAILED)",
+            ),
+        ),
     )
 
     HIGHLIGHTS: ClassVar[tuple[HighlightTypedDict, ...]] = ()
