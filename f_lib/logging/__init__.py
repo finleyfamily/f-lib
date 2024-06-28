156cb696b1338b2c941208044ce76b0c85b58deb
Kyle Finley <kyle@finley.sh>
2024-06-28 11:43:13 -0400

    export `setup_logging` (#95)

--- f_lib/logging/__init__.py
+++ f_lib/logging/__init__.py
@@ -8,12 +8,13 @@ from ._fluid_log_render import FluidLogRender
 from ._log_level import LogLevel
 from ._logger import Logger, LoggerSettings
 from ._prefix_adaptor import PrefixAdaptor
+from ._setup_logging import setup_logging
 
 __all__ = [
+    "ConsoleHandler",
     "DEFAULT_LOG_FORMAT",
     "DEFAULT_LOG_FORMAT_VERBOSE",
     "DEFAULT_STYLES",
-    "ConsoleHandler",
     "ExtendableHighlighter",
     "FluidLogRender",
     "HighlightTypedDict",
@@ -22,5 +23,6 @@ __all__ = [
     "LoggerSettings",
     "PrefixAdaptor",
     "settings",
+    "setup_logging",
     "utils",
 ]
